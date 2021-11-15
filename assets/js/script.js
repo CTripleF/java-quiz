2e307e67eac2ef4bac46aa06cc2566299aa0face
CTripleF <cjfabra1@gmail.com>
2021-11-14 23:25:45 -0500

    username form added, non funtional. also added end.html (currently unused)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var timerEl = document.getElementById('countdown');
 var scoreEl = document.getElementById('score')
-var questionEl = document.getElementsByClassName("answer-btn")
+//var formEl = document.getElementsById("user-form")
 var questionsArr= document.getElementsByClassName('question-card');
 var highScores=[];
 let currentScore = 0;
@@ -9,7 +9,7 @@ let questionNum = 0;
 const AWARD_POINTS = 100;
 
 
-var timeLeft = 5;
+var timeLeft = 60;
 if (timeLeft === 0){
   for(i=0; i < questionsArr; i++){
     questionsArr[i].style.display = "none";
@@ -31,10 +31,14 @@ function countdown() {
   1000);
 };
 
+
+
 function startQuiz(){
+
   questionsArr[0].style.display = "block";
 
   if (document.body.addEventListener){
+    //listen for click but only allow for one event. prevents answer change or scoring multiple times with single answer
     document.body.addEventListener('click',eventHandler,{once:true});
   }
   
@@ -47,50 +51,8 @@ function startQuiz(){
       checkAnswer(target);
     }
   }
-};
 
-
-// if (document.body.addEventListener){
-//   document.body.addEventListener('click',eventHandler,{once:true});
-// }
-
-// function eventHandler(e){
-//   e = e || window.event;
-//   var target = e.target || e.srcElement;
-//   if (target.className.match(/answer-btn/))
-//   {
-//     //an element with the answer-btn Class was clicked
-//     checkAnswer(target);
-//   }
-// }
-
-//check the selection for a correct answer, change background color, and update score/timer
-function checkAnswer(button) {
-  console.log(button)
-  if(button.className.match(/correct/)){
-    console.log('correct');
-    currentScore+= AWARD_POINTS;
-    scoreEl.textContent = 'Score: ' + currentScore;
-    button.classList.add('revealCorrect');
-    questionNum++;
-  }
-  else{
-    console.log('incorrect');
-    button.classList.add('revealIncorrect');
-    questionNum++;
-    timeLeft -=5;
-  }
-  console.log(currentScore)
-}
-
-
-// function removeQuestion(question){
-//   question--;
-//   questionsArr[question].style.display = "none";
-// }
-
-
-// use an interval to display questions
+  // use an interval to display questions
 var QuestionInterval = setInterval(hideShow, 3000);
 
 function hideShow(){
@@ -116,7 +78,7 @@ function hideShow(){
     clearInterval(QuestionInterval);
     //endQuiz();
   }
-  //ensure only current question os displayed
+  //ensure only current question is displayed
   if (questionNum >= 1 && questionNum <= 3){
     questionsArr[questionNum-1].style.display = "none";
   }
@@ -131,15 +93,42 @@ function hideShow(){
   }
 
 };
-
-
-function endQuiz(){
-  // ask for initials and display score, submit and save to local memory
-  // display option to clear local memory or go back and play again
-
 };
 
 
-countdown();
-startQuiz();
-// myFunction();
+//check the selection for a correct answer, change background color, and update score/timer
+function checkAnswer(button) {
+  console.log(button)
+  if(button.className.match(/correct/)){
+    console.log('correct');
+    currentScore+= AWARD_POINTS;
+    scoreEl.textContent = 'Score: ' + currentScore;
+    button.classList.add('revealCorrect');
+    questionNum++;
+  }
+  else{
+    console.log('incorrect');
+    button.classList.add('revealIncorrect');
+    questionNum++;
+    timeLeft -=5;
+  }
+  console.log(currentScore)
+}
+
+
+if (document.body.addEventListener){
+  
+  document.body.addEventListener('click',eventHandler,{once:true});
+}
+
+function eventHandler(e){
+  e = e || window.event;
+  var target = e.target || e.srcElement;
+  if (target.className.match(/begin-btn/))
+  {
+    e.preventDefault();
+    countdown();
+    startQuiz();
+  }
+}
+
